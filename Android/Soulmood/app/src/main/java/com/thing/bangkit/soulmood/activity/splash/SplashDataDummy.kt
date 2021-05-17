ae8876e4661175c3b44cd2375ad017601b594116
Thingkilia Finnatia Husin <vikasatibrahma@gmail.com>
2021-05-17 12:34:29 +0700

    update_chatbot_activity
    fix bug

--- Android/Soulmood/app/src/main/java/com/thing/bangkit/soulmood/activity/splash/SplashDataDummy.kt
+++ Android/Soulmood/app/src/main/java/com/thing/bangkit/soulmood/activity/splash/SplashDataDummy.kt
@@ -9,22 +9,21 @@ object SplashDataDummy {
         data.add(
             SplashData(
                 "Ceritakan Masalah Anda",
-                "Beban terasa lebih ringan dengan menceritakan masalah anda.",
+                "Beban terasa lebih ringan dengan saling berbagi cerita.",
                 R.drawable.splash_1
             )
         )
         data.add(
             SplashData(
                 "Aplikasi SoulMood Siap Membantu Anda", "SoulMood menyediakan layanan \n" +
-                        "Chatbot Cerdas yang bisa digunakan\n" +
-                        "untuk bercerita.", R.drawable.splash_2
+                        "Chatbot Cerdas dengan menjaga\n" +
+                        "informasi pribadi Anda.", R.drawable.splash_2
             )
         )
         data.add(
             SplashData(
-                "Layanan dapat diakses\n" +
-                        "secara gratis", "SoulMood menyediakan layanan \n" +
-                        "Chatbot secara gratis.", R.drawable.splash_3
+                "Layanan Chatbot Gratis", "Chatbot SoulMood dapat diakses \n" +
+                        "24 jam secara gratis.", R.drawable.splash_3
             )
         )
         return data
